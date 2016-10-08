098644fc7ba85c5cb8e206075e5bd36579970242
Oleksandr Khymenko <Oleksandr.Khymenko@evry.com>
2016-10-08 22:59:02 +0300

    Add images to invitees

--- client/src/app/projects/project/invitation/send/send-invitation.component.ts
+++ client/src/app/projects/project/invitation/send/send-invitation.component.ts
@@ -11,6 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SendInvitationComponent implements OnInit {
   project;
+
+  companies = [
+    {
+      name: 'Bank of America',
+      image: 'http://www.megaicons.net/static/img/icons_sizes/40/110/64/bank-of-america-icon.png'
+    },
+    {
+      name: 'Bank of America',
+      image: 'http://www.megaicons.net/static/img/icons_sizes/40/110/48/bank-of-america-icon.png'
+    },
+    {
+      name: 'Bank of America',
+      image: 'http://icons.iconarchive.com/icons/chrisbanks2/cold-fusion-hd/64/wellsfargo-2-icon.png'
+    },
+    {
+      name: 'Bank of America',
+      image: 'https://www.cebglobal.com/blogs/files/2014/01/PNCIcon-150x150.jpg'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
@@ -20,8 +39,13 @@ export class SendInvitationComponent implements OnInit {
       contactPerson: 'Per Person',
       loanAmount: '500m USD',
       marketIndustry: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis delectus dolor doloremque doloribus dolorum ducimus fuga incidunt. Accusantium cumque molestiae nesciunt officia quisquam sunt tempore. Assumenda consequuntur excepturi nesciunt rerum.'
-
     }
+  }
+
+  removeCompany(company) {
+    this.companies = this.companies.filter(data => {
+      return data !== company;
+    })
   }
 
 }
