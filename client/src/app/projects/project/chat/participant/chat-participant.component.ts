b99262d6673c0e902a0207b4d1381a678b3f8c5b
Oleksandr Khymenko <Oleksandr.Khymenko@evry.com>
2016-10-09 16:13:01 +0300

    Add chat participant component

---
+++ client/src/app/projects/project/chat/participant/chat-participant.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by Oleksandr.Khymenko on 09.10.2016.
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'chat-participant',
+    templateUrl: 'chat-participant.component.html'
+})
+export class ChatParticipantComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
