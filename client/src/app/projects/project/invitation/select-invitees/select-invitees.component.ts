27e099639d444a63ba0b82a46927fe7173bb4361
Oleksandr Khymenko <Oleksandr.Khymenko@evry.com>
2016-10-09 11:19:03 +0300

    add Selected invitees to invitation

--- client/src/app/projects/project/invitation/select-invitees/select-invitees.component.ts
+++ client/src/app/projects/project/invitation/select-invitees/select-invitees.component.ts
@@ -14,6 +14,25 @@ export class SelectInviteesComponent {
 
   selectedInvitees = [];
 
+  companies = [
+    {
+      name: 'Bank of America',
+      image: 'http://www.megaicons.net/static/img/icons_sizes/40/110/64/bank-of-america-icon.png'
+    },
+    {
+      name: 'Bank of America',
+      image: 'http://www.megaicons.net/static/img/icons_sizes/40/110/48/bank-of-america-icon.png'
+    },
+    {
+      name: 'Bank of America',
+      image: 'http://icons.iconarchive.com/icons/chrisbanks2/cold-fusion-hd/64/wellsfargo-2-icon.png'
+    },
+    {
+      name: 'Bank of America',
+      image: 'https://www.cebglobal.com/blogs/files/2014/01/PNCIcon-150x150.jpg'
+    }
+  ];
+
   constructor(private router: Router,
               private route: ActivatedRoute) {
       this.partners = [
@@ -23,6 +42,8 @@ export class SelectInviteesComponent {
           'Bank of Paper, Wilson & Bluemine LTD',
           'Bank of Housing Construction Inc'
       ];
+
+
   }
 
   nextTab(noInviteeModal) {
