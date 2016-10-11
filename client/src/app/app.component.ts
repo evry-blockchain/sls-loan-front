581f23c7cf3d5193c700b0c807cf53abac6af659
Oleksandr Khymenko <Oleksandr.Khymenko@evry.com>
2016-10-11 12:09:20 +0300

    Change scss of login component to use full height of page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component, ViewContainerRef } from '@angular/core';
+import { Component, ViewContainerRef, ViewEncapsulation } from '@angular/core';
 
 import {ApiService} from './shared';
 
 import '../style/app.scss';
 
 @Component({
-    selector: 'my-app', // <my-app></my-app>
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+  selector: 'my-app', // <my-app></my-app>
+  templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
     url = 'https://github.com/preboot/angular2-webpack';
