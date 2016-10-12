42dccd2eb87933aed4746bdf32854f0d591a92ac
Dmytro Zadorozhnyi <dmitriyzadorozniy@gmail.com>
2016-10-12 10:49:12 +0300

    list items name change

--- server/utils/prepare-list-data.js
+++ server/utils/prepare-list-data.js
@@ -2,6 +2,13 @@ module.exports = function(data) {
   if(data === ']') {
     return JSON.parse("[]")
   } else {
-    return JSON.parse(data);
+    return JSON.parse(data).map((item) => {
+      var temp = {};
+      for (var i in item) {
+        temp[i.charAt(0).toLowerCase() + i.slice(1)] = item[i];
+      }
+
+      return temp;
+    });
   }
 };
