cd28974e351ad44bc5f2181e59e696449095efb0
Oleksandr Khymenko <Oleksandr.Khymenko@evry.com>
2016-10-22 19:08:53 +0300

    Fix login when user is authenticated

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component, ViewContainerRef, ViewEncapsulation } from '@angular/core';
 import {ApiService} from './shared';
 
 import '../style/app.scss';
-
+import { AuthService } from "./auth/service/auth.service";
+import * as moment from 'moment';
 @Component({
   selector: 'my-app', // <my-app></my-app>
   templateUrl: './app.component.html',
@@ -14,9 +15,31 @@ export class AppComponent {
     url = 'https://github.com/preboot/angular2-webpack';
   private viewContainerRef: ViewContainerRef;
 
-  public constructor(viewContainerRef:ViewContainerRef) {
+  public constructor(viewContainerRef:ViewContainerRef, private  authService: AuthService) {
     // You need this small hack in order to catch application root view container ref
     this.viewContainerRef = viewContainerRef;
+
+    this.isUserLogedIn();
+
   }
+
+  isUserLogedIn() {
+    var matches = localStorage.getItem('bc.token');
+    if(!!matches) {
+      let object = JSON.parse(matches);
+      let dateString = moment(object.timestamp);
+      let  now = moment();
+      let diff = now.diff(dateString, 'hours');
+      let validHours = 15;
+      if (diff > validHours) {
+        this.authService.logout();
+        return;
+      }
+      this.authService.isLoggedIn$.next(true);
+      // compareTime(dateString, now); //to implement
+    }
+  }
+
+
 
 }
