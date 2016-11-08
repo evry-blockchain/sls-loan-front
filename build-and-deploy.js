02e693580e43ecf8c2322c96f34d1b3b13e3f331
Dmytro Zadorozhnyi <dmitriyzadorozniy@gmail.com>
2016-11-08 14:36:24 +0200

    build scripts for 76

--- build-and-deploy.js
+++ build-and-deploy.js
@@ -1,6 +1,13 @@
 require('shelljs/global');
 var rimraf = require('rimraf');
 var port = process.argv.slice(2)[0];
+var credsMap = {
+  '76': 'blockchain:oUVJ7ipL9U',
+  '68': 'vagrant:oUVJ7ipL9U',
+  '73': 'vagrant:oUVJ7ipL9U'
+};
+
+var creds = credsMap[port];
 
 console.log('Removing tmp and client dist folders...');
 
@@ -23,8 +30,8 @@ rimraf('.tmp', () => {
   exec('slc build -p');
 
   //deploy here
-  exec(`slc deploy -s sls-loan-node http://vagrant:oUVJ7ipL9U@195.250.62.${port}:8701`);
-  exec(`slc ctl -C http://vagrant:oUVJ7ipL9U@195.250.62.${port}:8701 env-set sls-loan-node MYCREDS=mycreds${port}.json`);
+  exec(`slc deploy -s sls-loan-node http://${creds}@195.250.62.${port}:8701`);
+  exec(`slc ctl -C http://${creds}@195.250.62.${port}:8701 env-set sls-loan-node MYCREDS=mycreds${port}.json`);
 
   console.log('Done');
 });
--- client/build-and-deploy.js
+++ client/build-and-deploy.js
@@ -1,5 +1,12 @@
 require('shelljs/global');
 var port = process.argv.slice(2)[0];
+var credsMap = {
+  '76': 'blockchain:oUVJ7ipL9U',
+  '68': 'vagrant:oUVJ7ipL9U',
+  '73': 'vagrant:oUVJ7ipL9U'
+};
+
+var creds = credsMap[port];
 
 exec('npm run build');
 
@@ -9,4 +16,4 @@ cp('package.json', 'dist/package.json');
 cd('dist');
 exec('slc build -p');
 
-exec(`slc deploy -s sls-loan-front http://vagrant:oUVJ7ipL9U@195.250.62.${port}:8701`);
+exec(`slc deploy -s sls-loan-front http://${creds}@195.250.62.${port}:8701`);
